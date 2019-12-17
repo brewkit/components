b137e19b48959cc2c6957cb29192b56263b32848
Mark Johnson <mark.johnson@MJohnson-VKHTD8.local>
2019-12-17 10:36:48 -0500

    lint fixes

--- src/components/StatusCard/Components/Compact/index.tsx
+++ src/components/StatusCard/Components/Compact/index.tsx
@@ -5,18 +5,18 @@ import { Props } from './types';
 import styles from './styles.scss';
 
 
-const CompactStatusCard = ({className, color, header, body, disabled, fill, ...otherProps}: Props) => {
+const CompactStatusCard = ({ className, color, header, body, disabled, fill, ...otherProps }: Props) => {
 
     const compactClasses = clsx(
         styles.wrapper,
         styles[`color--${color}`],
         disabled && styles.isDisabled,
         fill && styles.isFilled,
-        className
+        className,
     );
 
     return (
-        <div className={compactClasses} {...otherProps} >
+        <div className={compactClasses} {...otherProps}>
             <Typography align="center" variant="body1" color={color}>{header}</Typography>
             <Typography className={styles.cardBody} align="center" variant="body1">{body.toUpperCase()}</Typography>
         </div>
--- src/components/StatusCard/Components/Standard/index.tsx
+++ src/components/StatusCard/Components/Standard/index.tsx
@@ -6,14 +6,14 @@ import styles from './styles.scss';
 import { Props } from './types';
 
 
-const StandardStatusCard = ({className, color, icon, header, body, disabled, fill, ...otherProps}: Props) => {
+const StandardStatusCard = ({ className, color, icon, header, body, disabled, fill, ...otherProps }: Props) => {
 
     const standardClasses = clsx(
         styles.wrapper,
         styles[`color--${color}`],
         disabled && styles.isDisabled,
         fill && styles.isFilled,
-        className
+        className,
     );
 
     return (
