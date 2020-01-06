79f36a183798b1d744905a1e32843b2c19d061c4
joshrcook <joshua.cook@snapav.com>
2020-01-06 13:13:48 -0500

    Updated Dialog body

--- src/components/Dialog/components/DialogBody/index.tsx
+++ src/components/Dialog/components/DialogBody/index.tsx
@@ -1,18 +1,17 @@
 import React, { ReactElement } from 'react';
 import clsx from 'clsx';
-import Typography from '../../../Typography';
 import { Props } from './types';
 
 
 function DialogBody(props: Props): ReactElement {
-    const { className, children, ...otherProps } = props;
+    const { className, ...otherProps } = props;
     const classes = clsx(
         'brew-Dialog__body',
         className,
     );
 
     return (
-        <Typography as="div" className={classes} {...otherProps}>{children}</Typography>
+        <div className={classes} {...otherProps} />
     );
 }
 
