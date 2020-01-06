1f415ddaa558362f3036f0a775162d973cce8e1c
Lawren <lawrenipsum@gmail.com>
2020-01-06 13:52:09 -0500

    Update index.tsx

--- src/components/Dialog/components/DialogBody/index.tsx
+++ src/components/Dialog/components/DialogBody/index.tsx
@@ -3,16 +3,24 @@ import clsx from 'clsx';
 import { Props } from './types';
 
 
-function DialogBody(props: Props): ReactElement {
-    const { className, ...otherProps } = props;
+function DialogBody({
+    className,
+    ...otherProps
+}: Props): ReactElement {
+
+
     const classes = clsx(
         'brew-Dialog__body',
         className,
     );
 
+
     return (
         <div className={classes} {...otherProps} />
     );
+
+
 }
+
 
 export default DialogBody;
