b3d7ff8309bb127cdba2d4d459c5aa071256892c
joshrcook <joshua.cook@snapav.com>
2020-01-07 11:20:05 -0500

    Removed the portal component

--- src/components/Portal/index.tsx
+++
@@ -1,9 +0,0 @@
-import React, { ReactNode, ReactElement } from 'react';
-import ReactDOM from 'react-dom';
-
-
-function Portal({ children }: { children?: ReactNode }): ReactElement {
-    return ReactDOM.createPortal(children, document.body);
-}
-
-export default Portal;
