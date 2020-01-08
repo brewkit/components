82e73f8a7733f1bad3ebe1b6a2448b02dbe969b7
Alyssa DeLucca <alyssa.delucca@snapav.com>
2020-01-08 16:38:09 -0500

    finalized

--- src/components/Collapsible/test.tsx
+++ src/components/Collapsible/test.tsx
@@ -1,26 +1,27 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
 import Collapsible from './index';
-import { shallow } from 'enzyme';
+import {shallow} from 'enzyme';
+
 
 describe('Collapsible', () => {
 
     it('renders correctly', () => {
         const tree = renderer
-            .create(<Collapsible>Test</Collapsible>)
+            .create(<Collapsible anchor={(<div>Testing</div>)}>Test</Collapsible>)
             .toJSON();
         expect(tree).toMatchSnapshot();
     });
 
     it('mounts correctly', () => {
-        const collapsible = shallow(<Collapsible>Test</Collapsible>);
-        expect(collapsible.hasClass('brew-Collapsible')).toBe(true);
+        const collapsible = shallow(<Collapsible anchor={(<div>Testing</div>)}>Test</Collapsible>);
+        expect(collapsible.hasClass('brew-Collapsible')).toEqual(true);
     });
 
     it('correctly adds a className', () => {
         const className = `class-${Math.floor(Math.random() * 1000)}`;
-        const collapse = shallow(<Collapsible className={className}>Test</Collapsible>);
-        expect(collapse.hasClass(className)).toBe(true);
+        const collapsible = shallow(<Collapsible anchor={(<div>Testing</div>)} className={className}>Test</Collapsible>);
+        expect(collapsible.hasClass(className)).toEqual(true);
     });
 
 });
--- src/components/Collapsible/types.ts
+++ src/components/Collapsible/types.ts
@@ -11,7 +11,7 @@ export interface Props {
     /**
      * Static JSX, controls open/close functionality.
      */
-    anchor?: ReactNode,
+    anchor: ReactNode,
 
     /**
      * Boolean to determine if collapse action should occur.
