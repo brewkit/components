fb8565d9c8a3dc846ad39a498e554cbb2d27bff4
Mark Johnson <mark.johnson@MJohnsons-MacBook-Pro.local>
2020-01-28 09:05:12 -0500

    style(test): whitespace

--- src/components/Badge/test.tsx
+++ src/components/Badge/test.tsx
@@ -8,10 +8,14 @@ describe('Badge', () => {
 
 
     it('Renders correctly', () => {
-        const tree = renderer.create(<Badge><div>child</div></Badge>).toJSON();
+        const tree = renderer
+            .create(<Badge><div>child</div></Badge>)
+            .toJSON();
+
 
         expect(tree).toMatchSnapshot();
     });
+
 
     it('Has default classes', () => {
         const badge = shallow(<Badge><div>child</div></Badge>);
@@ -22,34 +26,41 @@ describe('Badge', () => {
             '.brew-Badge__element',
         ];
 
-        classes.forEach(className => {
+
+        classes.forEach((className: string) => {
             expect(badge.exists(className)).toBe(true);
         });
     });
 
+
     it('Adds custom class', () => {
         const badge = shallow(<Badge className="customizer"><div>child</div></Badge>);
 
+
         expect(badge.hasClass('customizer'));
     });
+
 
     it('Has circular and inline modifier class', () => {
         const badge = shallow(<div>child <Badge isCircular /></div>);
         const classes = ['brew-Badge--isCircular', 'brew-Badge--inline'];
 
-        classes.forEach(className => {
+
+        classes.forEach((className: string) => {
             expect(badge.hasClass(className));
         });
     });
+
 
     it('Changes default classes to prop modifiers', () => {
         const badge = shallow(<div>child <Badge anchorFrom="bottom-right" color="danger" /></div>);
         const classes = ['brew-Badge--anchorFrom-bottom-right', 'brew-Badge--color-danger'];
 
-        classes.forEach(className => {
+
+        classes.forEach((className: string) => {
             expect(badge.hasClass(className));
         });
-    })
+    });
 
 
 });
