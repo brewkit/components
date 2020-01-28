4cf449fc797a3fefc4c6ffe715731d9eb54b0e27
Mark Johnson <mark.johnson@MJohnsons-MacBook-Pro.local>
2020-01-28 14:06:44 -0500

    refactor(types): removed unused type interface

--- src/utilities/ScrollToListener/types.ts
+++ src/utilities/ScrollToListener/types.ts
@@ -1,17 +1,20 @@
 import { ReactNode } from 'react';
 
 
-export interface Bounding {
-    top?: number,
-    right?: number,
-    bottom?: number,
-    left?: number,
-}
-
-
 export interface Props {
+
+
+    /**
+     * Child element that the scroll to listener will be applied to
+     */
     children: ReactNode,
+
+
+    /**
+     * Function to call when the element comes into the viewport
+     */
     onScrollTo: () => void,
+
 
     /**
      * The element that is used as the viewport for checking visibility of the target.
@@ -20,6 +23,7 @@ export interface Props {
      */
     rootElement?: Element | null,
 
+
     /**
      * A string, formatted similarly to the CSS margin property.
      * Used to grow or shrink each side of the root element's bounding box before computing intersections
@@ -27,15 +31,19 @@ export interface Props {
      */
     rootMargin?: string,
 
+
     /**
      * Either a number or array of numbers indicating when the callback should be executed
      * 0 or [0] or [0, 0.25, 0.50, 0.75, 1]
      */
     rootThreshold?: number[] | number,
 
+
     /**
      * Specifies if you want the callback to continuously listen when scrolling down and up
      * Default specifies to only fire callback once, when scrolling into view initially
      */
     willListen?: boolean,
+
+
 }
