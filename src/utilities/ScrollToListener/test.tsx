bb6d6f172cb7a4bd038c317eac867b7ff2a8be60
Mark Johnson <mark.johnson@MJohnsons-MacBook-Pro.local>
2020-01-28 14:08:37 -0500

    test: added snapshot test

---
+++ src/utilities/ScrollToListener/test.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import ScrollToListener from './index';
+
+
+it('renders correctly', () => {
+    const tree = renderer
+        .create((
+            <ScrollToListener onScrollTo={(): boolean => true}>
+                <div>foo</div>
+            </ScrollToListener>
+        ))
+        .toJSON();
+    expect(tree).toMatchSnapshot();
+});
