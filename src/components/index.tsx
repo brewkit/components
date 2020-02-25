8fb4a4aa3baa2ffacbf69824458009ce43e4c6c6
Lawren <lawrenipsum@gmail.com>
2020-02-25 14:58:09 -0500

    feat: exposed material-ui/core via Mui component available at the root component level

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,3 +1,6 @@
+import * as Mui from '@material-ui/core';
+
+
 export { default as Button } from './Button';
 export { default as Flag } from './Flag';
 export { default as Icon } from './Icon';
@@ -5,3 +8,9 @@ export { default as Input } from './Input';
 export { default as Progress } from './Progress';
 export { default as Tooltip } from './Tooltip';
 export { default as Typography } from './Typography';
+
+
+/**
+ * Re-export MUI for easy use
+ */
+export { Mui };
