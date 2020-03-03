7a596a8dc30541e85225c93b3d3690e5405d1a34
Mark Johnson <mark.johnson@MJohnsons-MacBook-Pro.local>
2020-03-03 12:08:57 -0500

    test: added snapshot test

---
+++ src/components/Snackbar/test.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { mount } from 'enzyme';
+import renderer from 'react-test-renderer';
+import Snackbar from './index';
+
+
+it('renders correctly', () => {
+
+
+    const tree = renderer
+        .create(
+            <Snackbar
+                action={<div onClick={() => console.log('bar')}>Close</div>}
+                color="primary"
+                isOpen
+                message="foo"
+                onClose={() => console.log('foo')}
+                position="top-right"
+            />,
+            {
+                createNodeMock: node => document.createElement(node.type as any),
+            }).toJSON();
+
+
+    expect(tree).toMatchSnapshot();
+
+
+});
