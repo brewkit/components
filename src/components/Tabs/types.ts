090ef4eb072da2eedf70b96efe251a88ac06456e
Joshua Cook <joshuacook13@gmail.com>
2020-04-24 16:35:15 -0400

    Update types.ts

--- src/components/Tabs/types.ts
+++ src/components/Tabs/types.ts
@@ -1,7 +1,5 @@
-// import * as React from 'react';
+import React from 'react';
 import { StyledParentComponent } from '../types';
-
-// import { TabsProps } from '@material-ui/core/Tabs/Tabs.d.ts';
 
 export interface Props extends StyledParentComponent {
 
