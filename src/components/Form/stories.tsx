cbe655208b52d405508a6265cf4b037fa15d3dfb
Lawren <lawrenipsum@gmail.com>
2021-06-16 12:05:24 -0400

    fix: 🐛 cleaned up themes and form styles

--- src/components/Form/stories.tsx
+++ src/components/Form/stories.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { text, boolean } from '@storybook/addon-knobs';
+import { useForm } from 'react-hook-form';
 import Form from '@components/Form';
 import FormField from '@components/FormField';
 import FormSubmit from '@components/FormSubmit';
@@ -40,6 +40,44 @@ export const General = (): React.ReactElement => {
 
     return (
         <Form style={{ display: 'grid', gridGap: '1rem' }} onSubmit={handleSubmit} >
+            <FormField label="text1" name="text1" type="text" placeholder="text" autoComplete="new" validation={validation} />
+            <FormField label="Switch" name="switch" type="switch" validation={validation} />
+            <FormField label="Checkbox" name="checkbox" type="checkbox" validation={validation} />
+            <FormSubmit>Submit</FormSubmit>
+        </Form>
+    );
+
+}
+
+
+export const CustomFormMethods = (): React.ReactElement => {
+
+    const formMethods = useForm({ mode: 'onChange' });
+
+    const validation = {
+        required: {
+            value: true,
+            message: 'This field is required.',
+        },
+        minLength: {
+            value: 10,
+            message: 'A length of 10 or more is required.'
+        }
+    };
+
+
+    const handleSubmit = (values) => {
+        return new Promise((resolve, reject) => {
+            setTimeout(() => {
+                console.log(values);
+                resolve('foo');
+            }, 2000);
+        });
+    }
+
+
+    return (
+        <Form style={{ display: 'grid', gridGap: '1rem' }} onSubmit={handleSubmit} useFormMethods={formMethods}>
             <FormField label="text1" name="text1" type="text" placeholder="text" autoComplete="new" validation={validation} />
             <FormField label="Switch" name="switch" type="switch" validation={validation} />
             <FormField label="Checkbox" name="checkbox" type="checkbox" validation={validation} />
