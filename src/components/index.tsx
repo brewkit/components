9ec02124f38b1c259ae375e0fbb71a05bef26693
Pavle Popovic <ppopovic@control4.com>
2021-11-18 12:35:56 +0100

    more import changes

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -2,14 +2,14 @@ export { default as Alert, AlertProps } from './Alert';
 export { default as Badge, BadgeProps } from './Badge';
 export { default as Button, ButtonProps } from './Button';
 export { default as ButtonGroup } from './ButtonGroup';
-export { default as Checkbox } from './Checkbox/Checkbox';
+export { default as Checkbox, CheckboxProps } from './Checkbox';
 export { default as CircularProgress } from './CircularProgress';
 export { default as Form, FormProps } from './Form';
 export { default as FormField } from './FormField';
 export { default as FormSubmit } from './FormSubmit';
 export { default as LinearProgress } from './LinearProgress';
 export { default as Radio } from './Radio';
-export { default as TextField } from './TextField/TextField';
+export { default as TextField, TextFieldProps } from './TextField';
 
 
 // hooks
