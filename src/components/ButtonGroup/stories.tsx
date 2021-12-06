12435971ca656821810dc35fb94a9b4b3cbae6a6
beahoop <beahoopdesign@gmail.com>
2021-12-06 13:38:16 -0500

    adding sandbox to Button Group

--- src/components/ButtonGroup/stories.tsx
+++ src/components/ButtonGroup/stories.tsx
@@ -1,38 +1,29 @@
 import * as React from 'react';
-import {text, select, boolean} from '@storybook/addon-knobs';
 import ButtonGroup from '@components/ButtonGroup';
 import Button from '@components/Button';
 
 
 export default {
     component: ButtonGroup,
-    parameters: {
-        // componentSubtitle: <Flag color="success">Stable</Flag>,
-    },
     title: 'Inputs/ButtonGroup',
 };
 
+const defaultArgs = {
+    disabled: false,
+    disableElevation: true,
+    color: 'primary',
+    fullWidth: false,
+    variant: 'contained',
+    orientation: 'horizontal',
+};
 
-export const Sandbox = (): React.ReactElement => {
-
-    const color = select('color', ['default', 'primary', 'secondary', 'inherit'], 'primary');
-    const disabled = boolean('disabled', false);
-    const disableElevation = boolean('disableElevation', true);
-    const variant = select('variant', ['contained', 'outlined', 'text'], 'contained');
-    const orientation = select('orientation', ['vertical', 'horizontal'], 'horizontal');
-
-    return (
-        <ButtonGroup
-            variant={variant}
-            color={color}
-            disabled={disabled}
-            disableElevation={disableElevation}
-            orientation={orientation}
-        >
-            <Button>One</Button>
-            <Button>Two</Button>
-            <Button>Three</Button>
-        </ButtonGroup>
-    );
-
+export const Sandbox = (args: any): React.ReactElement => (
+    <ButtonGroup {...args}>
+        <Button>One</Button>
+        <Button>Two</Button>
+        <Button>Three</Button>
+    </ButtonGroup>);
+Sandbox.args = {
+    ...defaultArgs,
+    color: 'primary',
 };
