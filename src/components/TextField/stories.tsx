8353367f839479d30281f7d4a508a3fd8b5b981c
Sarah Bea Hooper <70774362+beahoop@users.noreply.github.com>
2021-12-07 13:53:33 -0500

    fix: 🐛 Resolve Storybook sandboxes issues
    fix: 🐛 Update ButtonGroup Storybook sandbox
    fix: 🐛 Update Checkbox Storybook sandbox
    fix: 🐛 Update TextField Storybook sandbox

--- src/components/TextField/stories.tsx
+++ src/components/TextField/stories.tsx
@@ -1,5 +1,4 @@
 import * as React from 'react';
-import { text, boolean } from '@storybook/addon-knobs';
 import MenuItem from '@material-ui/core/MenuItem';
 
 
@@ -7,7 +6,7 @@ import MenuItem from '@material-ui/core/MenuItem';
  * This is an annoying hack to get Storybook to properly parse the documentation when the underlying component
  * is wrapped in a HOC. Expect to do/see this on every component with MUI theming options.
  */
-import { TextField  } from './TextField';
+import { TextField } from './TextField';
 
 
 export default {
@@ -18,61 +17,48 @@ export default {
     title: 'Inputs/TextField',
 };
 
-
-export const Sandbox = (): React.ReactElement => {
-
-    const label = text('label', 'Form Label');
-    const placeholder = text('placeholder', 'Some placeholder text');
-    const helperText = text('helperText', 'Some helper text');
-    const error = boolean('error', false);
-    const fullWidth = boolean('fullWidth', false);
-    const disabled = boolean('disabled', false);
-    const multiline = boolean('multiline', false);
-
-    return (
-        <TextField
-            error={error}
-            label={label}
-            placeholder={placeholder}
-            disabled={disabled}
-            fullWidth={fullWidth}
-            helperText={helperText}
-            multiline={multiline}
-        />
-    );
-
+const defaultArgs = {
+    disabled: false,
+    error: false,
+    fullWidth: false,
+    helperText: 'Some helper text',
+    label: 'Form Label',
+    multiline: false,
+    placeholder: 'Some placeholder text',
 };
 
 
-export const Types = (): React.ReactElement => {
+export const Sandbox = (args: any): React.ReactElement => <TextField {...args}/>
+Sandbox.args = {
+    ...defaultArgs,
+};
 
-    return (
-        <div style={{ display: 'grid', gridGap: '1rem' }}>
-            <TextField label="Text" type="text" />
-            <TextField label="Password" type="password" />
-            <TextField label="Search" type="search" />
-            <TextField label="Number" type="number" />
-            <TextField
-                label="Multiline"
-                multiline
-                rows={4}
-                defaultValue="Default Value"
-            />
-            <TextField
-                id="outlined-select-currency"
-                select
-                label="Select"
-                helperText="Please select your number."
-                variant="outlined"
-            >
-                <MenuItem value="One">One</MenuItem>
-                <MenuItem value="Two">Two</MenuItem>
-                <MenuItem value="Three">Three</MenuItem>
-            </TextField>
-        </div>
-    );
 
-}
+export const Types = (): React.ReactElement => (
+    <div style={{ display: 'grid', gridGap: '1rem' }}>
+        <TextField label="Text" type="text" />
+        <TextField label="Password" type="password" />
+        <TextField label="Search" type="search" />
+        <TextField label="Number" type="number" />
+        <TextField
+            defaultValue="Default Value"
+            label="Multiline"
+            multiline
+            rows={4}
+        />
+        <TextField
+            helperText="Please select your number."
+            id="outlined-select-currency"
+            label="Select"
+            select
+            variant="outlined"
+        >
+            <MenuItem value="One">One</MenuItem>
+            <MenuItem value="Two">Two</MenuItem>
+            <MenuItem value="Three">Three</MenuItem>
+        </TextField>
+    </div>
+);
 
 
 export const Select = (): React.ReactElement => {
@@ -83,17 +69,17 @@ export const Select = (): React.ReactElement => {
         { label: 'two', value: 2 },
         { label: 'three', value: 3 },
         { label: 'four', value: 4 },
-    ]
+    ];
 
     return (
         <TextField
             defaultValue=""
-            select
-            label="Select"
             helperText="Please select your number."
-            variant="outlined"
+            label="Select"
             options={options}
+            select
+            variant="outlined"
         />
     );
 
-}
+};
