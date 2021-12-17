adc6a991c2401c7b0529a5db8a64f605a787de19
Eric Woodward <2585736+ItsEricWoodward@users.noreply.github.com>
2021-12-17 17:15:55 -0500

    fix: 🐛 Fix build issues due to capitalization problems

--- src/components/Alert/Alert.Story.tsx
+++ src/components/Alert/Alert.Story.tsx
@@ -3,12 +3,10 @@ import { Story } from '@storybook/react';
 import Alert from './Alert';
 import { AlertProps } from './Alert.types';
 
-
 export default {
     component: Alert,
     title: 'Data Display/Alert',
 };
-
 
 export const Sandbox: Story<AlertProps> = (args) => <Alert {...args} />;
 Sandbox.args = {
@@ -16,42 +14,75 @@ Sandbox.args = {
     severity: 'warning',
     variant: 'standard',
     square: false,
-    children: 'I am a description of the alert!'
+    children: 'I am a description of the alert!',
 };
 
-
-export const General : Story<AlertProps> = (args) => (
+export const General: Story<AlertProps> = (args) => (
     <div style={{ display: 'grid', gridGap: '1rem' }}>
-        <Alert severity="error" title="This is an error alert with no call to action." />
-        <Alert severity="warning" title="This is an warning alert with no call to action." />
-        <Alert severity="info" title="This is an info alert with no call to action." />
-        <Alert severity="success" title="This is an success alert with no call to action." />
+        <Alert
+            {...args}
+            severity="error"
+            title="This is an error alert with no call to action."
+        />
+        <Alert
+            {...args}
+            severity="warning"
+            title="This is an warning alert with no call to action."
+        />
+        <Alert
+            {...args}
+            severity="info"
+            title="This is an info alert with no call to action."
+        />
+        <Alert
+            {...args}
+            severity="success"
+            title="This is an success alert with no call to action."
+        />
     </div>
 );
 
-
-export const Filled : Story<AlertProps> = (args) => (
+export const Filled: Story<AlertProps> = (args) => (
     <div style={{ display: 'grid', gridGap: '1rem' }}>
-        <Alert severity="error" title="This is an error alert with no call to action." variant="filled" />
-        <Alert severity="warning" title="This is an warning alert with no call to action." variant="filled" />
-        <Alert severity="info" title="This is an info alert with no call to action." variant="filled" />
-        <Alert severity="success" title="This is an success alert with no call to action." variant="filled" />
+        <Alert
+            {...args}
+            severity="error"
+            title="This is an error alert with no call to action."
+            variant="filled"
+        />
+        <Alert
+            {...args}
+            severity="warning"
+            title="This is an warning alert with no call to action."
+            variant="filled"
+        />
+        <Alert
+            {...args}
+            severity="info"
+            title="This is an info alert with no call to action."
+            variant="filled"
+        />
+        <Alert
+            {...args}
+            severity="success"
+            title="This is an success alert with no call to action."
+            variant="filled"
+        />
     </div>
 );
 
-
-export const WithDescription : Story<AlertProps> = (args) => (
+export const WithDescription: Story<AlertProps> = (args) => (
     <div style={{ display: 'grid', gridGap: '1rem' }}>
-        <Alert severity="error" title="Error">
+        <Alert severity="error" title="Error" {...args}>
             This is an error alert with no call to action.
         </Alert>
         <Alert severity="warning" title="Warning">
             This is an warning alert with no call to action.
         </Alert>
-        <Alert severity="info" title="Info">
+        <Alert severity="info" title="Info" {...args}>
             This is an info alert with no call to action.
         </Alert>
-        <Alert severity="success" title="Success">
+        <Alert severity="success" title="Success" {...args}>
             This is an success alert with no call to action.
         </Alert>
     </div>
