39f8b8521a369ed8636a7bceac16e888a5eee9b2
Eric Woodward <eric.woodward@snapav.com>
2021-12-17 17:18:03 -0500

    fix: 🐛 Trying to fix capitalization issues

--- src/components/Alert/Alert.Types.ts
+++
@@ -1,9 +0,0 @@
-import { AlertProps as MUIAlertProps } from '@material-ui/lab/Alert';
-
-export type AlertProps = MUIAlertProps & {
-    /**
-     * An abstraction for `<AlertTitle />`
-     */
-    title?: string,
-
-};
