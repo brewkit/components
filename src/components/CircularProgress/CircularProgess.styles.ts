3163eabbbd07758e7c589e6617604fedd8c8fbee
Pavle Popovic <ppopovic@control4.com>
2021-12-20 17:35:28 +0100

    problem with cherry pick last week

--- src/components/CircularProgress/CircularProgess.styles.ts
+++ src/components/CircularProgress/CircularProgess.styles.ts
@@ -1,8 +1,6 @@
 import { makeStyles } from '@material-ui/core/styles';
 
-
 const useStyles = makeStyles({
-
     root: {
         display: 'inline-block',
         position: 'relative',
@@ -11,8 +9,6 @@ const useStyles = makeStyles({
             opacity: 0.25,
         },
     },
-
 });
-
 
 export default useStyles;
