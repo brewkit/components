a48f055a50fa1f1455f4bac9d5805aa988394bca
adelucca23 <asd2@pct.edu>
2022-01-05 14:54:06 -0500

    Component to use material icons

---
+++ themes/cerveza/components/Icon.ts
@@ -0,0 +1,28 @@
+import { color, size } from '../variables';
+
+
+const Icon = {
+
+    props: {
+
+        MuiIcon: {},
+
+    },
+
+    overrides: {
+
+        MuiButton: {
+
+            /**
+             * general styles
+             */
+            root: {},
+
+            },
+
+        },
+
+};
+
+
+export default Icon;
