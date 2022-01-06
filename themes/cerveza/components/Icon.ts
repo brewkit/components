fe0d3ef90c6e9b1bac0741ed90a739a3800bf37b
Alyssa DeLucca <30605992+alyssadee@users.noreply.github.com>
2022-01-06 10:06:25 -0500

    feat: 🎸 Icons

---
+++ themes/cerveza/components/Icon.ts
@@ -0,0 +1,28 @@
+import { color, size } from '../variables';
+
+
+const Icon = {
+
+    props: {
+
+        MuiIcon: {},
+
+    },
+
+    overrides: {
+
+        MuiIcon: {
+
+            /**
+             * general styles
+             */
+            root: {},
+
+            },
+
+        },
+
+};
+
+
+export default Icon;
