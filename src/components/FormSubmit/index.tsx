3d8f5063a7d24a1dfd4f6dcbe87f46fa4bc6b857
Pavle Popovic <ppopovic@control4.com>
2022-01-24 15:58:01 +0100

    fix: 🐛 Moving otherProps upwards
    to prevent override of loading and disabled
    
    ✅ Closes: OVRC-13851

--- src/components/FormSubmit/index.tsx
+++ src/components/FormSubmit/index.tsx
@@ -3,37 +3,34 @@ import { useFormContext } from 'react-hook-form';
 import Button from '../Button';
 import { Props } from './types';
 
-
 /**
  * `FormSubmit` is a submit button with additional bindings to a parent `Form` to allow for better control and
  * validation. It automatically disables itself while it's parent `Form` is incomplete.
  */
-export const FormSubmit = React.forwardRef(({
-    children = 'Submit',
-    ...otherProps
-}: Props, ref: React.Ref<any>): React.ReactElement => {
+export const FormSubmit = React.forwardRef(
+    (
+        { children = 'Submit', ...otherProps }: Props,
+        ref: React.Ref<any>,
+    ): React.ReactElement => {
+        const { formState } = useFormContext();
 
-
-    const { formState } = useFormContext();
-
-
-    return (
-        <Button
-            disabled={!formState.isValid || formState.isSubmitting || formState.isValidating}
-            loading={formState.isSubmitting}
-            ref={ref}
-            type="submit"
-            {...otherProps}
-        >
-            {children}
-        </Button>
-    );
-
-
-});
-
+        return (
+            <Button
+                {...otherProps}
+                disabled={
+                    !formState.isValid ||
+                    formState.isSubmitting ||
+                    formState.isValidating
+                }
+                loading={formState.isSubmitting}
+                ref={ref}
+                type="submit">
+                {children}
+            </Button>
+        );
+    },
+);
 
 FormSubmit.displayName = 'FormSubmit';
-
 
 export default FormSubmit;
