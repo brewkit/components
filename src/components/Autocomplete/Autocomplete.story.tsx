a91a999efc3804052de78249ff6687a139685b93
Pavle Popovic <ppopovic@control4.com>
2022-03-11 17:17:36 +0100

    refactor: 💡 Adding search icon when type is search
    ✅ Closes: OVRC-14663

--- src/components/Autocomplete/Autocomplete.story.tsx
+++ src/components/Autocomplete/Autocomplete.story.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import { Story } from '@storybook/react';
-
+import Button, { ButtonProps } from '../Button';
 
 /**
  * This is an annoying hack to get Storybook to properly parse the documentation when the underlying component
@@ -18,8 +18,8 @@ export default {
 };
 
 interface FilmOptionType {
-    title: string,
-    year: number,
+    title: string;
+    year: number;
 }
 
 // Top 20 films as rated by IMDb users. http://www.imdb.com/chart/top
@@ -29,7 +29,7 @@ const top20Films = [
     { title: 'The Godfather: Part II', year: 1974 },
     { title: 'The Dark Knight', year: 2008 },
     { title: '12 Angry Men', year: 1957 },
-    { title: 'Schindler\'s List', year: 1993 },
+    { title: "Schindler's List", year: 1993 },
     { title: 'The Lord of the Rings: The Return of the King', year: 2003 },
     { title: 'Pulp Fiction', year: 1994 },
     { title: 'The Good, the Bad and the Ugly', year: 1966 },
@@ -41,40 +41,33 @@ const top20Films = [
     { title: 'Star Wars: Episode V - The Empire Strikes Back', year: 1980 },
     { title: 'The Matrix', year: 1999 },
     { title: 'Goodfellas', year: 1990 },
-    { title: 'One Flew Over the Cuckoo\'s Nest', year: 1975 },
+    { title: "One Flew Over the Cuckoo's Nest", year: 1975 },
     { title: 'Seven Samurai', year: 1954 },
     { title: 'Se7en', year: 1995 },
 ];
 
 const defaultArgs = {
     options: top20Films,
-    getOptionLabel: (option: unknown): string => (option as FilmOptionType).title,
+    getOptionLabel: (option: unknown): string =>
+        (option as FilmOptionType).title,
 };
 
 const flatArgs = {
     options: top20Films.map((option) => option.title),
 };
 
-
-export const Sandbox: Story<AutocompleteProps<any, false, false, false>> =
-    (args: any): React.ReactElement => <Autocomplete {...args} />;
-
-Sandbox.args = {
-    ...defaultArgs,
-};
-
-
-export const Sandbox: Story<AutocompleteProps<any, false, false, false>> =
-    (args: any): React.ReactElement => <Autocomplete {...args} />;
+export const Sandbox: Story<AutocompleteProps<unknown, false, false, false>> =
+    function sandbox(args): React.ReactElement {
+        return <Autocomplete {...args} />;
+    };
 
 Sandbox.args = {
     ...defaultArgs,
 };
 
-
-
-
-export const General: Story<ButtonProps> = (args) => <Button {...args} />;
+export const General: Story<ButtonProps> = function general(args) {
+    return <Button {...args} />;
+};
 
 General.args = {
     ...defaultArgs,
@@ -82,58 +75,58 @@ General.args = {
     variant: 'text',
 };
 
+export function Types(): React.ReactElement {
+    return (
+        <div style={{ display: 'grid', gridGap: '1rem' }}>
+            <Autocomplete
+                {...defaultArgs}
+                getOptionLabel={(option: unknown): string =>
+                    (option as FilmOptionType).title
+                }
+                id="standard"
+                label="standard"
+                options={top20Films}
+            />
 
-export const Types = (): React.ReactElement => (
-    <div style={{ display: 'grid', gridGap: '1rem' }}>
-        <Autocomplete
-            getOptionLabel={(option: unknown): string => (option as FilmOptionType).title}
-            id="standard"
-            label="standard"
-            options={top20Films}
-        />
+            <Autocomplete
+                {...defaultArgs}
+                clearOnBlur
+                handleHomeEndKeys
+                id="combo-box"
+                label="combo box"
+                selectOnFocus
+            />
 
-        <Autocomplete
-            {...defaultArgs}
-            clearOnBlur
-            handleHomeEndKeys
-            id="combo-box"
-            label="combo box"
-            selectOnFocus
-        />
+            <Autocomplete
+                {...defaultArgs}
+                clearOnEscape
+                id="clear-on-escape"
+                label="clearOnEscape"
+            />
 
-        <Autocomplete
-            {...defaultArgs}
-            clearOnEscape
-            id="clear-on-escape"
-            label="clearOnEscape"
-        />
+            <Autocomplete {...flatArgs} id="flat-demo" label="flatDemo" />
 
-        <Autocomplete
-            {...flatArgs}
-            id="flat-demo"
-            label="flatDemo"
-        />
+            <Autocomplete
+                {...defaultArgs}
+                autoComplete
+                id="auto-complete"
+                includeInputInList
+            />
 
-        <Autocomplete
-            {...defaultArgs}
-            autoComplete
-            id="auto-complete"
-            includeInputInList
-        />
+            <Autocomplete
+                {...defaultArgs}
+                // freeSolo
+                id="free-solo-demo"
+                label="standard"
+            />
 
-        <Autocomplete
-            {...defaultArgs}
-            freeSolo
-            id="free-solo-demo"
-            label="standard"
-        />
-
-        <Autocomplete
-            {...defaultArgs}
-            freeSolo
-            id="search"
-            label="search"
-            type="search"
-        />
-    </div>
-);
+            <Autocomplete
+                {...defaultArgs}
+                // freeSolo
+                id="search"
+                label="search"
+                type="search"
+            />
+        </div>
+    );
+}
--- src/components/Autocomplete/Autocomplete.tsx
+++ src/components/Autocomplete/Autocomplete.tsx
@@ -1,12 +1,12 @@
-
 import * as React from 'react';
 import MuiAutocomplete from '@material-ui/lab/Autocomplete';
+import InputAdornment from '@material-ui/core/InputAdornment';
 
+import SearchIcon from '@material-ui/icons/Search';
 // import { motion, AnimatePresence } from 'framer-motion';
-import TextField from '../TextField';
+import TextField from '@material-ui/core/TextField';
 import { AutocompleteProps } from './Autocomplete.types';
 import useStyles from './styles';
-
 
 /**
  * Autocomplete fields let users enter and edit text, and select items from a pop-up list.
@@ -17,41 +17,49 @@ import useStyles from './styles';
  *
  * - Adds a search icon when `type="search"`. Styles can be overridden using `BkAutocomplete.searchIcon`.
  */
-const AutocompleteInner = <
+function AutocompleteInner<
     T,
     Multiple extends boolean | undefined,
     DisableClearable extends boolean | undefined,
     FreeSolo extends boolean | undefined,
-> (
-        {
-            InputProps,
-            label,
-            renderInput,
-            type,
-            ...otherProps
-        }: AutocompleteProps<T, Multiple, DisableClearable, FreeSolo>,
-        ref: React.ForwardedRef<any>,
-    ): React.ReactElement<any, string | React.JSXElementConstructor<any>> | null => {
-
+>(
+    {
+        InputProps,
+        // label,
+        renderInput,
+        type,
+        ...otherProps
+    }: AutocompleteProps<T, Multiple, DisableClearable, FreeSolo>,
+    ref: React.ForwardedRef<any>,
+): React.ReactElement<any, string | React.JSXElementConstructor<any>> | null {
     const classes = useStyles();
 
-    const renderFunction = renderInput
-        ? renderInput
-        : (params: any): React.ReactNode => (
+    const icon =
+        type === 'search' ? (
+            <InputAdornment position="start">
+                <SearchIcon />
+            </InputAdornment>
+        ) : null;
+
+    const renderFunction =
+        renderInput ||
+        ((params: any): React.ReactNode => (
             <TextField
                 {...params}
-                InputProps={{ ...InputProps }}
-                label={label}
+                InputProps={{
+                    ...InputProps,
+                    startAdornment: icon,
+                }}
+                // label={label}
                 type={type}
                 variant="outlined"
             />
-        );
+        ));
 
     /**
      * generate select options from options prop. Will use MenuItem if desktop and native treatments if mobile.
      * keep this, or something like it?
      */
-
 
     return (
         <MuiAutocomplete
@@ -61,13 +69,10 @@ const AutocompleteInner = <
             renderInput={renderFunction}
         />
     );
-};
-
+}
 
 export const Autocomplete = React.forwardRef(AutocompleteInner);
 
 Autocomplete.displayName = 'Autocomplete';
 
-
 export default Autocomplete;
-
